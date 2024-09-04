8919e3f4e7a18f9f4bdfce6869a9a5953e58baba
Quangpig123 <www.quangmap123@gmail.com>
2024-09-04 14:47:05 +0700

    Xong bai 5

---
+++ BaiTap05/BaiTap05/Controllers/TheLoaiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTap05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 28";
+            ViewBag.Thang = "Tháng 02";
+            ViewData["Nam"] = "Năm 2024";
+            return View();
+        }
+    }
+}
