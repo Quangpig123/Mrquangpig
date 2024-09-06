59ec37772264198f69c4d1ec1eb644dccd6d6bb7
Quangpig123 <www.quangmap123@gmail.com>
2024-09-06 16:39:59 +0700

    Xong BTVN

---
+++ BTVNSlide20/BTVNSlide20/Controllers/Tuan02Controller.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BTVNSlide20.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Đặng Minh Quang";
+            ViewBag.MSSV = "1822040944";
+            ViewBag.Nam = DateTime.Now.Year;
+            return View();
+        }
+        public ActionResult MayTinh(int a, int b, string pheptinh)
+        {
+            double ketqua = 0;
+
+           
+            switch (pheptinh)
+            {
+                case "cong":
+                    ketqua = a + b;
+                    break;
+                case "tru":
+                    ketqua = a - b;
+                    break;
+                case "nhan":
+                    ketqua = a * b;
+                    break;
+                case "chia":
+               
+                    if (b != 0)
+                    {
+                        ketqua = (double)a / b;
+                    }
+                    else
+                    {
+                        ViewBag.Loi = "Không thể chia cho 0";
+                    }
+                    break;
+                default:
+                    ViewBag.Loi = "Phép tính không hợp lệ";
+                    break;
+            }
+
+         
+            if (ViewBag.Loi == null)
+            {
+                ViewBag.KetQua = ketqua;
+            }
+
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
