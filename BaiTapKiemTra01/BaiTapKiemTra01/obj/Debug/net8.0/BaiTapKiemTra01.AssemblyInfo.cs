16cb280b3e03ebef36bb4ce250fcef0bdf1e826b
Quangpig123 <www.quangmap123@gmail.com>
2024-09-14 15:09:39 +0700

    Xong bai 1 them minh chung

--- BaiTapKiemTra01/BaiTapKiemTra01/obj/Debug/net8.0/BaiTapKiemTra01.AssemblyInfo.cs
+++ BaiTapKiemTra01/BaiTapKiemTra01/obj/Debug/net8.0/BaiTapKiemTra01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaiTapKiemTra01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce7f10fbf9812d828b65aac069aec7fca3c9ef7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06840eb597fdeb120d7a701b9e44945ed3b19f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaiTapKiemTra01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaiTapKiemTra01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
